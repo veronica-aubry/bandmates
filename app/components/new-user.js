1886a7737cebc4400cbdae2b50d6b2754dc98288
Marika <mballely@gmail.com>
2016-04-12 09:20:16 -0700

    add saveUser function

--- app/components/new-user.js
+++ app/components/new-user.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveUser() {
+      var params = {
+        name: this.get('name') ? this.get('name') : "Tom",
+        image: this.get('image') ? this.get('image') : "http://cos.h-cdn.co/assets/15/25/1434726176-tom.jpg",
+        activeUser: true
+      };
+      this.sendAction('saveUser', params);
+    }
+  }
 });
