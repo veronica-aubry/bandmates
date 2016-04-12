6ca6b377e65fdec95f7dc3802ed2c0e1138fcdc7
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-12 16:15:28 -0700

    adds backgroudn box css

--- app/routes/new.js
+++ app/routes/new.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
   actions: {
     saveUser(params) {
       var newUser = this.store.createRecord('user', params);
@@ -9,5 +8,4 @@ export default Ember.Route.extend({
       this.transitionTo('new');
     }
   }
-
 });
