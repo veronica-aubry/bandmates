4925666165c500f8f0730b684b4704163e424028
Blake Scott <blakerscott@gmail.com>
2016-04-12 16:03:52 -0700

    add invite button to user detail template

--- app/components/user-detail.js
+++ app/components/user-detail.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  inviteMate: Ember.inject.service(),
+
+  actions: {
+        addBandMate(answer) {
+          this.get('inviteMate').add(answer);
+        }
+      }
+  });
