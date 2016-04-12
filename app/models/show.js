9ba5bbe138b74b92cd53a9ce204e002a0e29f427
Veronica Alley <veronica.aubry.alley@gmail.com>
2016-04-12 11:37:18 -0700

    add match tile component

--- app/models/show.js
+++ app/models/show.js
@@ -1,9 +1,8 @@
 import Model from 'ember-data/model';
 
 export default Model.extend({
-  title: DS.attr(),
   venue: DS.attr(),
   band: DS.attr(),
   date: DS.attr(),
-  attendees: DS.hasMany('user' { async: true })
+  attendees: DS.hasMany('user', { async: true })
 });
