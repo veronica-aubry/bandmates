7c6e038ae01c030c025e26de2f9161e406586337
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-12 11:44:44 -0700

    fixes bug

--- app/models/show.js
+++ app/models/show.js
@@ -5,5 +5,5 @@ export default Model.extend({
   venue: DS.attr(),
   band: DS.attr(),
   date: DS.attr(),
-  attendees: DS.hasMany('user' { async: true })
+  attendees: DS.hasMany('user', { async: true }),
 });
