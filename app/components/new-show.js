31f584216e82fba0a73a2c2bbfea340d318a69dd
Marika <mballely@gmail.com>
2016-04-13 09:37:24 -0700

    add saveShow function

--- app/components/new-show.js
+++ app/components/new-show.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveShow() {
+      var params = {
+        venue: this.get('venue') ? this.get('venue') : "House of Blues",
+        band: this.get('band') ? this.get('band') : "They Might Be Giants",
+        date: this.get('date') ? this.get('date') : "05-14"
+      };
+      this.sendAction('saveShow', params);
+    }
+  }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('show');
+  },
+  actions: {
+    saveShow(params) {
+      var newShow = this.store.createRecord('show', params);
+      newShow.save();
+      this.transitionTo('index');
+    }
+  }
 });
