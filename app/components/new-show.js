15417efbb8d3beaf3a613de8d44ea37c806d5e38
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-13 10:16:23 -0700

    adds images to bands

--- app/components/new-show.js
+++ app/components/new-show.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
       var params = {
         venue: this.get('venue') ? this.get('venue') : "House of Blues",
         band: this.get('band') ? this.get('band') : "They Might Be Giants",
+        image: this.get('image'),
         date: this.get('date') ? this.get('date') : "05-14"
       };
       this.sendAction('saveShow', params);
