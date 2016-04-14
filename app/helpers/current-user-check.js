ea1db200d09308bb29b1968f0e841f85eaf7b28d
Michael Schennum <mikeschenphoto@gmail.com>
2016-04-14 16:18:10 -0700

    wont let me merge

--- app/helpers/current-user-check.js
+++ app/helpers/current-user-check.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function currentUserCheck(params, session) {
   var userId = params[0].get('uid');
-
+  console.log(session.uid);
   if (userId === session.uid) {
     return true;
   }
