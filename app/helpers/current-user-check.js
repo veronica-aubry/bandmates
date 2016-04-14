18f3e86bb06c738868d008564b5af286b99984cb
Marika <mballely@gmail.com>
2016-04-14 16:47:46 -0700

    fix invite bug

--- app/helpers/current-user-check.js
+++ app/helpers/current-user-check.js
@@ -4,10 +4,9 @@ export function currentUserCheck(params) {
   var userId = params[0].id;
   var activeUserId = params[1].id;
 
-  if (userId !== activeUserId) {
-    return Ember.String.htmlSafe("<button class='btn' {{action 'addBandMate' user show}}>Invite this bandmate</button>"); 
+  if (userId == activeUserId) {
+    return 'display:none';
   }
-
 }
 
 
