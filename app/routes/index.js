80a87f4d18aa1eafc7ae14d943010726018d450f
Marika <mballely@gmail.com>
2016-04-14 09:53:14 -0700

    add user model to show route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model() {
-  //   return Ember.RSVP.hash({
-  //     shows: this.store.findAll('show'),
-  //     user: (if (!this.get('session').get('uid')) {
-  //       return this.find('user', c76cd9da-08cd-4741-a4d5-5ce4ec532e1d);
-  //     } else {
-  //       return this.find('user', this.get('session').get('uid'));
-  //     })
-  //   });
-  // }
-
   beforeModel() {
     if (!this.get('session').get('uid')) {
       this.transitionTo('login')
--- app/routes/show.js
+++ app/routes/show.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    if (!this.get('session').get('uid')) {
+      this.transitionTo('login')
+    }
+  },
+  model(params) {
+    return Ember.RSVP.hash({
+      show: this.store.findRecord('show', params.show_id),
+      user: this.store.find('user', this.get('session').get('uid'))
+    });
+  }
 });
